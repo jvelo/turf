47fc43248d030d9b82868ccb9152d844ea1f0bed
Jérôme Velociter <jerome@velociter.fr>
2015-09-04 19:04:20 +0200

    Started scraper script

---
+++ scrape.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/env groovy
+@Grapes(
+  @Grab(group='org.jsoup', module='jsoup', version='1.8.3')
+)
+
+import groovy.util.XmlSlurper
+import org.jsoup.Jsoup
+
+// Fetch the table section @ https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+
+def raw = new URL("https://en.wikipedia.org/w/api.php?format=xml&action=parse&page=ISO_3166-1&prop=text&section=4").text
+def api = new XmlSlurper().parseText(raw)
+def doc = Jsoup.parse(api.parse.text.toString())
+def table = doc.select("table");
+
+def rows = table.select("tr");
+rows.each {
+  println  it.select("td").first()?.text() ?: ""
+}
+
+assert table.size() == 1
+
